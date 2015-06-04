c439d1d367d3ad0f1a511c715cffdbe617dd3a3e
Priit Kallas <kallaspriit@gmail.com>
2015-06-04 09:38:00 +0300

    Initial commit.

---
+++ src/Calculator.js
@@ -0,0 +1,5 @@
+function sum(a, b) {
+	'use strict';
+
+	return a + b;
+}
---
+++ test/CalculatorSpec.js
@@ -0,0 +1,9 @@
+describe('Calculator', function() {
+
+	it('should sum given values', function() {
+		var result = sum(2, 5);
+
+		expect(result).toEqual(7);
+	});
+
+});
